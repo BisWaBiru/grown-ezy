89673dd35daa4cb08a539084979186901ad1831e
BisWaBiru <biswakarmabiru5@gmail.com>
2025-05-31 22:31:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// script.js - Global JavaScript (if any)
+
+// Example: Smooth scroll for anchor links (if you add any)
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// You can add common utility functions here if needed by multiple tools,
+// though for this project, most JS will be embedded in individual tool HTML files
+// for easier copy-pasting and modularity as per the request.
+
+// console.log("Global script.js loaded.");
